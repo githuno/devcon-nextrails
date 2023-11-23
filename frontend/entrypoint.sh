caaab601821ea41e11261a2c30e2facd95eeca7d
githuno <clas.warehouse@gmail.com>
2023-11-23 21:34:57 +0900

    edit

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,10 +4,10 @@ set -e # エラーがでたらスクリプトを終了する
 
 echo -e "script---> \\n︙\\n︙\\n"
 echo -e "whoami : {`whoami`}\\n"
-echo -e "${CONTAINER_FRONT} is moving...\\n"
-mv -f /tmp/${CONTAINER_FRONT}/* /tmp/${CONTAINER_FRONT}/.[^\.]* .
+echo -e "${CONTAINER_FRONT} is copying...\\n"
+cp -rf /tmp/${CONTAINER_FRONT}/* .
 rm -rf /tmp/${CONTAINER_FRONT}
-echo -e "${CONTAINER_FRONT} is moved\\n"
+echo -e "${CONTAINER_FRONT} is copyed\\n"
 echo -e "︙\\n︙\\n---> script done !! "
 
 # # Then exec the container's main process (what's set as CMD in the Dockerfile).
